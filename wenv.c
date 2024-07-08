503cdfc270c73a1ca977fecb6eef09fef2d7c5ae
Eliah Kagan <degeneracypressure@gmail.com>
2024-07-08 00:08:40 -0400

    Do way fewer allocations
    + Make some other parts more elegant.

--- wenv.c
+++ wenv.c
@@ -1,11 +1,17 @@
 // Windows utility to print all environment variables, sorted by name.
 
+#include <limits.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <wchar.h>
 
 #define FREE_AND_SET_NULL(ptr) do { free(ptr); ptr = NULL; } while (false)
+
+static inline size_t minimum(size_t x, size_t y)
+{
+    return y < x ? y : x;
+}
 
 static void *xcalloc(size_t count, size_t size)
 {
@@ -19,7 +25,7 @@ static void *xcalloc(size_t count, size_t size)
 static void *xcmemcpy(void *__restrict dest, const void *__restrict src,
                       size_t count, size_t size)
 {
-    if (size != 0 && count > (size_t)-1 / size) {
+    if (size != 0 && count > SIZE_MAX / size) {
         abort(); // This is to avoid arithmetic overflow.
     }
     return memcpy(dest, src, count * size);
@@ -34,25 +40,34 @@ static size_t count_vars(wchar_t **envp)
     return count;
 }
 
-static wchar_t *dup_name(wchar_t const *var)
+static inline size_t name_length(wchar_t *const var)
 {
-    wchar_t *end = wcschr(var, L'=');
-    size_t len = (end ? (size_t)(end - var) : wcslen(var)); // Not very elegant.
-    wchar_t *name = xcalloc(len + 1, sizeof(name[0]));
-    wmemcpy(name, var, len + 1);
-    return name;
+    wchar_t const* end = wcschr(var, L'=');
+    return end ? (size_t)(end - var) : wcslen(var);
 }
 
-static int compare_names(void const *lhspv, void const *rhspv)
+static int compare_names(wchar_t const *lhs, wchar_t const *rhs)
 {
-    wchar_t **lhsp = lhspv;
-    wchar_t **rhsp = rhspv;
-    wchar_t *__restrict lhs_name = dup_name(*lhsp);
-    wchar_t *__restrict rhs_name = dup_name(*rhsp);
-    int result = wcscmp(lhs_name, rhs_name); // Maybe wcsicmp would be better.
-    FREE_AND_SET_NULL(rhs_name);
-    FREE_AND_SET_NULL(lhs_name);
-    return result;
+    size_t lhs_len = name_length(lhs);
+    size_t rhs_len = name_length(rhs);
+
+    int prefix_result = wcsncmp(lhs, rhs, minimum(lhs_len, rhs_len));
+
+    if (prefix_result != 0) {
+        return prefix_result;
+    }
+    if (lhs_len < rhs_len) {
+        return -1;
+    }
+    if (lhs_len > rhs_len) {
+        return 1;
+    }
+    return 0;
+}
+
+static int compare_names_for_qsort(void const *lhspv, void const *rhspv)
+{
+    return compare_names(*(wchar_t const **)lhspv, *(wchar_t const **)rhspv);
 }
 
 int wmain(int argc, wchar_t **argv, wchar_t **envp)
@@ -61,7 +76,7 @@ int wmain(int argc, wchar_t **argv, wchar_t **envp)
     wchar_t **vars = xcalloc(count + 1, sizeof(vars[0]));
     xcmemcpy(vars, envp, count + 1, sizeof(vars[0]));
 
-    qsort(vars, count, sizeof(vars[0]), compare_names);
+    qsort(vars, count, sizeof(vars[0]), compare_names_for_qsort);
     for (wchar_t **varp = vars; *varp; ++varp) {
         _putws(*varp);
     }
