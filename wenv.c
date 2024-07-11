de473c7d8543ca1c28a3e9db683b45b759e5bb5c
Eliah Kagan <degeneracypressure@gmail.com>
2024-07-11 06:14:50 -0400

    Explain the point of sorting
    Since this is a Windows-only tool.

--- wenv.c
+++ wenv.c
@@ -3,6 +3,10 @@
 // Pass -s or --sort for a case-sensitive sort. Otherwise the variables are
 // shown in the order they appear in the environment block, which on Windows
 // should be a case-insensitive sort.
+//
+// The case-sensitive sort is sometimes useful for comparison to the output of
+// utilities that sort environment variables from Unix-like systems that way
+// because they expect them to be wholly unsorted.
 
 #include <limits.h>
 #include <stdbool.h>
