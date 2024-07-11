60dcb7baf9fccb9057a20caaad9a74f16fbfa37b
Eliah Kagan <degeneracypressure@gmail.com>
2024-07-11 06:07:59 -0400

    Make the (extra) sorting optional

--- wenv.c
+++ wenv.c
@@ -1,4 +1,8 @@
-// Windows utility to print all environment variables, sorted by name.
+// Windows utility to print all environment variables.
+//
+// Pass -s or --sort for a case-sensitive sort. Otherwise the variables are
+// shown in the order they appear in the environment block, which on Windows
+// should be a case-insensitive sort.
 
 #include <limits.h>
 #include <stdbool.h>
@@ -27,6 +31,37 @@ static void *xcmemcpy(void *__restrict dest, const void *__restrict src,
         abort(); // This is to avoid arithmetic overflow.
     }
     return memcpy(dest, src, count * size);
+}
+
+struct config {
+    bool help;
+    bool sort;
+    wchar_t const* unrecognized;
+};
+
+static inline bool is_match(wchar_t const *arg,
+                            wchar_t const *short_opt,
+                            wchar_t const *long_opt)
+{
+    return wcscmp(arg, short_opt) == 0
+        || wcscmp(arg, long_opt) == 0;
+}
+
+static void parse_config(int argc, wchar_t **argv, struct config *cfgp)
+{
+    cfgp->help = false;
+    cfgp->sort = false;
+    cfgp->unrecognized = NULL;
+
+    for (int i = 1; i < argc; ++i) {
+        if (is_match(argv[i], L"-h", L"--help")) {
+            cfgp->help = true;
+        } else if (is_match(argv[i], L"-s", L"--sort")) {
+            cfgp->sort = true;
+        } else if (!cfgp->unrecognized) {
+            cfgp->unrecognized = argv[i];
+        }
+    }
 }
 
 static size_t count_vars(wchar_t **envp)
@@ -68,19 +103,46 @@ static int compare_names_for_qsort(void const *lhspv, void const *rhspv)
     return compare_names(*(wchar_t const **)lhspv, *(wchar_t const **)rhspv);
 }
 
-int wmain(int argc, wchar_t **argv, wchar_t **envp)
+static void print_vars(wchar_t **vars)
 {
-    (void)argc;
-    (void)argv;
+    for (wchar_t** varp = vars; *varp; ++varp) {
+        _putws(*varp);
+    }
+}
 
+static void print_vars_sorted(wchar_t** envp)
+{
     size_t count = count_vars(envp);
     wchar_t **vars = xcalloc(count + 1, sizeof(vars[0]));
     xcmemcpy(vars, envp, count + 1, sizeof(vars[0]));
 
     qsort(vars, count, sizeof(vars[0]), compare_names_for_qsort);
-    for (wchar_t **varp = vars; *varp; ++varp) {
-        _putws(*varp);
-    }
+    print_vars(vars);
 
     free(vars);
+}
+
+int wmain(int argc, wchar_t **argv, wchar_t **envp)
+{
+    struct config cfg = { 0 };
+    parse_config(argc, argv, &cfg);
+
+    if (cfg.help) {
+        fwprintf(stderr, L"Usage:  %s [--sort|-s]\n", argv[0]);
+        fwprintf(stderr, L"\n  Shows environment variables, one per line.\n");
+        fwprintf(stderr, L"\n  Pass --sort or -s to sort case-sensitively.\n");
+        return cfg.unrecognized ? EXIT_FAILURE : EXIT_SUCCESS;
+    }
+    if (cfg.unrecognized) {
+        fwprintf(stderr, L"%s: error: unrecognized option: %s\n", argv[0], cfg.unrecognized);
+        return EXIT_FAILURE;
+    }
+
+    if (cfg.sort) {
+        print_vars_sorted(envp);
+    } else {
+        print_vars(envp);
+    }
+
+    return EXIT_SUCCESS;
 }
